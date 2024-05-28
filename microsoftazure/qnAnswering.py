65bd1e3d06973d2cf85aee9b3d130de970f03514
shiyun36 <111891177+shiyun36@users.noreply.github.com>
2024-05-28 23:38:26 +0200

    Create qnAnswering.py

---
+++ microsoftazure/qnAnswering.py
@@ -0,0 +1,6 @@
+## Create a user support bot solution on Microsoft Azure using a combination of two core services:
+# Azure AI Language: includes a custom question answering feature that enables you to create a knowledge base of question and answer pairs that can be queried using natural language input.
+# Azure AI Bot Service: provides a framework for developing, publishing, and managing bots on Azure.
+# Use Azure AI Language Studio to create, train, publish, and manage question answering projects.
+
+###https://microsoftlearning.github.io/mslearn-ai-fundamentals/Instructions/Labs/07-question-answering.html
